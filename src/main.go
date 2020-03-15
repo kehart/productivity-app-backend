065c5da56f3219d55ad91e4f09b685a333a8d29a
Katherine Hart <kasiahart@yahoo.ca>
2020-03-15 15:09:43 -0400

    Partially added access to mongo

--- src/main.go
+++ src/main.go
@@ -8,13 +8,21 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	uuid "github.com/satori/go.uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/mgo.v2"
 )
 
+type UserController struct{
+	session 	*mgo.Session
+}
+
+
 type user struct {
-	FirstName 	string `json:"first_name"`
-	LastName  	string `json:"last_name"`
-	ID			uuid.UUID `json:"id"`
+	FirstName 	string `json:"first_name" bson:"first_name"`
+	LastName  	string `json:"last_name" bson:"last_name"`
+	ID			primitive.ObjectID `json:"id" bson:"_id"`
 }
 
 type allUsers []user
@@ -23,11 +31,24 @@ var users = allUsers{
 	{
 		FirstName: "Kasia",
 		LastName:  "Hart",
-		ID: uuid.Must(uuid.NewV4()),
+		ID: primitive.NewObjectID(),
 	},
 }
 
-func createUser(w http.ResponseWriter, r *http.Request) {
+// session *mgo.session
+
+func getSession() *mgo.Session {
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://localhost") // default mongo port
+
+	// Check if connection error
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+func (uc UserController) createUser(w http.ResponseWriter, r *http.Request) {
 	var newUser user
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -35,27 +56,57 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.Unmarshal(reqBody, &newUser)
-	newUser.ID = uuid.Must(uuid.NewV4())
+	uc.session.DB("admin-db").C("users").Insert(newUser)
+
+	newUser.ID = primitive.NewObjectID()
 	users = append(users, newUser)
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(newUser)
 }
 
-func getAllUsers(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) getAllUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-func updateUser(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) getSingleUser(w http.ResponseWriter, r *http.Request) {
+	// Some hacky conversions to get right type for id
+	userID := []byte(mux.Vars(r)["id"])
+	rv := bson.RawValue{
+		Type: bsontype.ObjectID,
+		Value: 	userID,
+	}
+	objId, success := rv.ObjectIDOK(); if !success {
+		fmt.Println("error")
+	}
+	// Set up var that will hold requested user data
+	var user user
+
+	_, err := ioutil.ReadAll(r.Body); if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		return
+	})
+	// search for user
+	err = uc.session.DB("admin-db").C("users").FindId(objId).One(&user); if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc UserController) updateUser(w http.ResponseWriter, r *http.Request) {
 	userID := mux.Vars(r)["id"]
 	var updatedUser user
 
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter data with the user data in order to update")
+		fmt.Println("error")
 	}
 	json.Unmarshal(reqBody, &updatedUser)
 
+	// search for user
 	for i, singleUser := range users {
 		if singleUser.ID.String() == userID {
 			singleUser.FirstName = updatedUser.FirstName
@@ -66,7 +117,7 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func deleteUser(w http.ResponseWriter, r *http.Request) {
+func (uc UserController) deleteUser(w http.ResponseWriter, r *http.Request) {
 	userID := mux.Vars(r)["id"]
 
 	for i, singleUser := range users {
@@ -82,13 +133,17 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	s := getSession()
+	uc := UserController{session:s}
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 
-	router.HandleFunc("/users", createUser).Methods("POST")
-	router.HandleFunc("/users", getAllUsers).Methods("GET")
-	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
-	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/users", uc.createUser).Methods("POST")
+	router.HandleFunc("/users", uc.getAllUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", uc.getSingleUser).Methods("GET")
+	router.HandleFunc("/users/{id}", uc.updateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", uc.deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", router)) // create server
 }
